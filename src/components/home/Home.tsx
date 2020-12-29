817a06267b5653b9ab3f46eb2ed4ae769282f66c
Jeremy Lavigne <lavigne.jeremy@post.com>
2020-12-29 16:29:06 +0100

    Scraped upcoming matches.
    Get betexplorer data,
    Only onClick for now.
    Axios and Cheerio to help.

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from '../button/Button';
+
+import nextM from '../../utils/nextM';
+
+import { Match } from '../../ts/app_types';
 
 const Home: React.FC = (): JSX.Element => {
-    console.log('home');
+    const [nextMatches, setNextMatches] = useState<Array<Match>>([]);
+
+    const handleScrap = () => {
+        nextM('https://www.betexplorer.com/soccer/england/premier-league/').then((res) => setNextMatches(res));
+    };
 
     return (
         <div>
             <h1>My React and TypeScript App!</h1>
             <div>
-                <p>Hello</p>
+                <Button onClick={handleScrap}>Scrap it</Button>
+            </div>
+            <div>
+                {nextMatches.map((match) => (
+                    <li key={`${match.homeTeam}-${match.date}`}>
+                        {match.date}
+                        {match.homeTeam} vs {match.awayTeam}: {match.oddH}/{match.oddD}/{match.oddA}
+                    </li>
+                ))}
             </div>
         </div>
     );
