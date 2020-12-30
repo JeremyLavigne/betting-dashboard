1a5bff6aad995e047e76daab0c2097e764af64d5
Jeremy Lavigne <lavigne.jeremy@post.com>
2020-12-30 12:17:51 +0100

    Previous matches fetched from web.
    football-data.co.uk website.
    Fetch csv as string and convert to JSon with only necessary data.

--- src/ts/app_types.ts
+++ src/ts/app_types.ts
@@ -1,4 +1,4 @@
-// Object type when scraping in Betexplorer for upcoming match
+// Object type when scraping in Betexplorer for upcoming matches
 export interface Match {
     homeTeam: string | undefined;
     awayTeam: string | undefined;
@@ -6,4 +6,25 @@ export interface Match {
     oddH: string | undefined;
     oddD: string | undefined;
     oddA: string | undefined;
+}
+
+// Object type when scraping in Football-data.co.uk for previous matches
+export interface MatchPlayed {
+    Date: string;
+    HomeTeam: string;
+    AwayTeam: string;
+    FTHG: number;
+    FTAG: number;
+    FTR: string;
+    HS: number;
+    AS: number;
+    HST: number;
+    AST: number;
+    HC: number;
+    AC: number;
+    BbAvH: number;
+    BbAvD: number;
+    BbAvA: number;
+    'BbAv>2.5': number;
+    'BbAv<2.5': number;
 }
